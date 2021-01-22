ccb7c470b6198cff96ba8564875e7f1e951ec9fa
inksharedsolutions <webdev.inkshared@gmail.com>
2021-01-22 10:39:11 +0800

    banner little changes might change later

--- src/Services/Home/components/Banner.tsx
+++ src/Services/Home/components/Banner.tsx
@@ -1,180 +1,19 @@
 import React from 'react';
 import '../assets/banner.scss';
 
-const Banner = () =>{
-    return(
-        <body>
-            <main className="site-wrapper">
-                <div className="pt-table desktop-768">
-                    <div className="pt-tablecell page-home relative bgded">
-                        <div className="overlay"></div>
-
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-xs-12 col-md-offset-1 col-md-10 col-lg-offset-2 col-lg-8">
-                                    <div className="page-title  home text-center">
-                                    <span className="heading-page"> Welcome to My Page
-                                    </span>
-                                        <p className="mt20">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-                                    </div>
-
-                                    <div className="hexagon-menu clear">
-                                        <div className="hexagon-item">
-                                            <div className="hex-item">
-                                                <div></div>
-                                                <div></div>
-                                                <div></div>
-                                            </div>
-                                            <div className="hex-item">
-                                                <div></div>
-                                                <div></div>
-                                                <div></div>
-                                            </div>
-                                            <a href="https://enationalelectronics.com" target="_blank" className="hex-content">
-                                                <span className="hex-content-inner">
-                                                    <span className="icon">
-                                                        <i className="fa fa-universal-access"></i>
-                                                    </span>
-                                                    <span className="title">Welcome</span>
-                                                </span>
-                                                <svg viewBox="0 0 173.20508075688772 200" height="200" width="174" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path></svg>
-                                            </a>
-                                        </div>
-                                        <div className="hexagon-item">
-                                            <div className="hex-item">
-                                                <div></div>
-                                                <div></div>
-                                                <div></div>
-                                            </div>
-                                            <div className="hex-item">
-                                                <div></div>
-                                                <div></div>
-                                                <div></div>
-                                            </div>
-                                            <a href="https://enationalelectronics.com" target="_blank" className="hex-content">
-                                                <span className="hex-content-inner">
-                                                    <span className="icon">
-                                                        <i className="fa fa-bullseye"></i>
-                                                    </span>
-                                                    <span className="title">About</span>
-                                                </span>
-                                                <svg viewBox="0 0 173.20508075688772 200" height="200" width="174" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path></svg>
-                                            </a>
-                                        </div>
-                                        <div className="hexagon-item">
-                                            <div className="hex-item">
-                                                <div></div>
-                                                <div></div>
-                                                <div></div>
-                                            </div>
-                                            <div className="hex-item">
-                                                <div></div>
-                                                <div></div>
-                                                <div></div>
-                                            </div>
-                                            <a href="https://enationalelectronics.com" target="_blank" className="hex-content">
-                                                <span className="hex-content-inner">
-                                                    <span className="icon">
-                                                        <i className="fa fa-braille"></i>
-                                                    </span>
-                                                    <span className="title">Services</span>
-                                                </span>
-                                                <svg viewBox="0 0 173.20508075688772 200" height="200" width="174" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path></svg>
-                                            </a>    
-                                        </div>
-                                        <div className="hexagon-item">
-                                            <div className="hex-item">
-                                                <div></div>
-                                                <div></div>
-                                                <div></div>
-                                            </div>
-                                            <div className="hex-item">
-                                                <div></div>
-                                                <div></div>
-                                                <div></div>
-                                            </div>
-                                            <a href="https://enationalelectronics.com" target="_blank" className="hex-content">
-                                                <span className="hex-content-inner">
-                                                    <span className="icon">
-                                                        <i className="fa fa-id-badge"></i>
-                                                    </span>
-                                                    <span className="title">Resume</span>
-                                                </span>
-                                                <svg viewBox="0 0 173.20508075688772 200" height="200" width="174" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path></svg>
-                                            </a>
-                                        </div>
-                                        <div className="hexagon-item">
-                                            <div className="hex-item">
-                                                <div></div>
-                                                <div></div>
-                                                <div></div>
-                                            </div>
-                                            <div className="hex-item">
-                                                <div></div>
-                                                <div></div>
-                                                <div></div>
-                                            </div>
-                                            <a href="https://enationalelectronics.com" target="_blank" className="hex-content">
-                                                <span className="hex-content-inner">
-                                                    <span className="icon">
-                                                        <i className="fa fa-life-ring"></i>
-                                                    </span>
-                                                    <span className="title">Works</span>
-                                                </span>
-                                                <svg viewBox="0 0 173.20508075688772 200" height="200" width="174" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path></svg>
-                                            </a>
-                                        </div>
-                                        <div className="hexagon-item">
-                                            <div className="hex-item">
-                                                <div></div>
-                                                <div></div>
-                                                <div></div>
-                                            </div>
-                                            <div className="hex-item">
-                                                <div></div>
-                                                <div></div>
-                                                <div></div>
-                                            </div>
-                                            <a href="https://enationalelectronics.com" target="_blank" className="hex-content">
-                                                <span className="hex-content-inner">
-                                                    <span className="icon">
-                                                        <i className="fa fa-clipboard"></i>
-                                                    </span>
-                                                    <span className="title">Testimonials</span>
-                                                </span>
-                                                <svg viewBox="0 0 173.20508075688772 200" height="200" width="174" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path></svg>
-                                            </a>
-                                        </div>
-                                        <div className="hexagon-item">
-                                            <div className="hex-item">
-                                                <div></div>
-                                                <div></div>
-                                                <div></div>
-                                            </div>
-                                            <div className="hex-item">
-                                                <div></div>
-                                                <div></div>
-                                                <div></div>
-                                            </div>
-                                            <a href="https://enationalelectronics.com" target="_blank" className="hex-content">
-                                                <span className="hex-content-inner">
-                                                    <span className="icon">
-                                                        <i className="fa fa-map-signs"></i>
-                                                    </span>
-                                                    <span className="title">Contact</span>
-                                                </span>
-                                                <svg viewBox="0 0 173.20508075688772 200" height="200" width="174" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M86.60254037844386 0L173.20508075688772 50L173.20508075688772 150L86.60254037844386 200L0 150L0 50Z" fill="#1e2530"></path></svg>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </main>
-        </body>
-    )
-}
-
+class Banner extends React.Component {
+    render() {
+      return (
+        <section className="hero is-primary is-fullheight">
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <h1 className="title">Hello, Bulma!</h1>
+              <h2 className="subtitle">This is react app feat. Bulma</h2>
+            </div>
+          </div>
+        </section>
+      );
+    }
+  }
+  
 export default Banner;
